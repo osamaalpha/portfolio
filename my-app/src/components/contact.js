2381614d96d4861be07e4a0127b1abc0a62e9570
Osama Gozlan <38697285+osamaalpha@users.noreply.github.com>
2025-04-20 02:27:50 +0200

    Update contact.js

--- my-app/src/components/contact.js
+++ my-app/src/components/contact.js
@@ -30,7 +30,7 @@ export const Contact = () => {
           <h2>
             <a
               style={{ color: "black" }}
-              href="https://drive.google.com/file/d/1srixuxLpw9Ay_9voQ_IT6Xk-6LPAqzcH/view?usp=sharing"
+              href="https://drive.google.com/file/d/1vGE2Rbb5pm6GEvkJZNHJhTbDWqAxnUVk/view?usp=sharing"
             >
               Resume
             </a>
